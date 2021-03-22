b43f2273bffd8024a41700891603e7debb884cee
Marcus <marcus.gamboa@gmail.com>
2021-03-22 13:18:44 -0600

    Connected redux to App. Mapped State/Dispatch to Timer Component

--- src/index.js
+++ src/index.js
@@ -2,47 +2,83 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, bindActionCreators } from "redux";
 import { Provider, connect } from "react-redux";
 
-// Start Redux
-const reducer1 = (state = "hello", action) => {
+/* Start Redux */
+const TIMER = "TIMER";
+
+//Timer Reducer & Action
+const timerInitialState = {
+  count: 0,
+};
+
+const timerReducer = (state = timerInitialState, action) => {
   switch (action.type) {
+    case TIMER:
+      console.log("boop");
     default:
-      return state;
+      return timerInitialState;
   }
 };
-const rootReducer = combineReducers({
-  reducer1,
+
+const timerAction = () => ({
+  type: TIMER,
 });
 
+// Create Store & Root Reducer
+
+const rootReducer = combineReducers({
+  timerReducer,
+});
 const store = createStore(rootReducer);
 
-//Start React
+/* Start React */
+
+// Timer Component
+class Timer extends React.Component {
+  render() {
+    return (
+      <div className="timerContainer">
+        <h1>This is a timer</h1>
+        <h2>{this.props.count}</h2>
+        <button onClick={this.props.timerAction}>start</button>
+      </div>
+    );
+  }
+}
+
+const mapStateToPropsTimer = (state) => ({
+  count: state.timerReducer.count,
+});
+
+const mapDispatchToPropsTimer = (dispatch) =>
+  bindActionCreators({ timerAction: timerAction }, dispatch);
+
+const ConnectedTimer = connect(
+  mapStateToPropsTimer,
+  mapDispatchToPropsTimer
+)(Timer);
+
+//Base App
 class App extends React.Component {
   render() {
     console.log(this.props);
     return (
       <div className="base">
         <div className="configureSections">
-          <h1>{this.props.propName}</h1>
-          <h1>hi</h1>
+          <h1>Session Length</h1>
+          <h1>Break</h1>
         </div>
-        <h1>Timer</h1>
+        <ConnectedTimer />
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => ({
-  propName: state.reducer1,
-});
-
-const ConnectedApp = connect(mapStateToProps)(App);
-
 ReactDOM.render(
   <Provider store={store}>
-    <ConnectedApp />
+    <App />
   </Provider>,
   document.querySelector("#root")
 );
