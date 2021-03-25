37b13d2bc196007fbf747acb3d31bfa20aad6d38
Marcus <marcus.gamboa@gmail.com>
2021-03-24 18:52:22 -0600

    Scratching Redux

--- src/index.js
+++ src/index.js
@@ -2,102 +2,78 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import { Provider, connect } from "react-redux";
-import thunkMiddleware from "redux-thunk";
-
-/* Start Redux */
-const INCREMENT = "INCREMENT";
-const DECREMENT = "DECREMENT";
-
-//Timer Reducer & Action
-const timerInitialState = {
-  sessionTime: 25,
-  breakTime: 5,
-  count: 0,
-};
-
-const timerReducer = (state = timerInitialState, action) => {
-  var stateCopy;
-  switch (action.type) {
-    case INCREMENT:
-      return { ...state, sessionTime: ++state.sessionTime };
-    case DECREMENT:
-      return { ...state, sessionTime: --state.sessionTime };
-    default:
-      return state;
-  }
-};
-
-const addSessionTime = () => ({
-  type: INCREMENT,
-});
-
-const subtractSessionTime = () => ({
-  type: DECREMENT,
-});
-
-// Create Store & Root Reducer
-
-const rootReducer = combineReducers({
-  timerReducer,
-});
-
-const store = createStore(rootReducer);
-
-/* Start React */
-class App extends React.Component {
+/* Component to adjust the times */
+class AdjustTimeContainer extends React.Component {
   render() {
     return (
-      <div className="base">
-        <div className="configureSections">
-          {/* Section to Configure Session Length */}
-          <div className="sessionAndBreakContainers">
-            <h3>Session Length</h3>
-            <h2>{this.props.sessionTime}</h2>
-            <div>
-              <button onClick={this.props.addSessionTime}>+</button>
-              <button onClick={this.props.subtractSessionTime}>-</button>
-            </div>
-          </div>
-          {/* Section to Configure Break Length */}
-          <div className="sessionAndBreakContainers">
-            <h3>Break Length</h3>
-            <h2>{this.props.breakTime}</h2>
-            <div>
-              <button onClick={this.props.addSessionTime}>+</button>
-              <button onClick={this.props.subtractSessionTime}>-</button>
-            </div>
-          </div>
-        </div>
-        {/* Countdown Timer */}
-        <div className="timerContainer">
-          <h1>This is a timer</h1>
-          <h2>{this.props.sessionTime}</h2>
-          <div className="timerButtonsContainer">
-            <button className="playPauseButton">Play/Pause</button>
-            <button className="resetButton">Reset</button>
-          </div>
+      <div className="sessionAndBreakContainers">
+        <h3>{this.props.name}</h3>
+        <h2>{this.props.sessionLength}</h2>
+        <div>
+          <button onClick={this.props.changeSessionLength}>+</button>
+          <button>-</button>
         </div>
       </div>
     );
   }
 }
 
-const mapStateToPropsApp = (state) => ({
-  sessionTime: state.timerReducer.sessionTime,
-  breakTime: state.timerReducer.breakTime,
-});
+class Timer extends React.Component {
+  render() {
+    return (
+      <div className="timerContainer">
+        <h1>This is a timer</h1>
+        <h2>{this.props.sessionLength}</h2>
+        <div className="timerButtonsContainer">
+          <button className="playPauseButton">Play/Pause</button>
+          <button className="resetButton" onClick={this.props.resetAction}>
+            Reset
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
 
-const mapDispatchToPropsApp = { addSessionTime, subtractSessionTime };
+/* Base App */
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sessionLength: 25,
+      runningClock: 25,
+      breakLength: 5,
+    };
+    this.changeSessionLength = this.changeSessionLength.bind(this);
+  }
+  changeSessionLength = (e) => {
+    console.log(e.target.dataset);
+  };
 
-const ConnectedApp = connect(mapStateToPropsApp, mapDispatchToPropsApp)(App);
+  render() {
+    let sessionLength = this.state.sessionLength;
+    let breakLength = this.state.breakLength;
+    return (
+      <div className="base">
+        <div className="configureSections">
+          <AdjustTimeContainer
+            name="Session Length"
+            sessionLength={sessionLength}
+            dataLabel="session"
+            changeSessionLength={this.changeSessionLength}
+          />
+          <AdjustTimeContainer
+            name="Break Length"
+            sessionLength={breakLength}
+            changeSessionLength={this.changeSessionLength}
+          />
+        </div>
+        <Timer sessionLength={this.state.sessionLength} />
+      </div>
+    );
+  }
+}
 
 /* Render */
 
-ReactDOM.render(
-  <Provider store={store}>
-    <ConnectedApp />
-  </Provider>,
-  document.querySelector("#root")
-);
+ReactDOM.render(<App />, document.querySelector("#root"));
